47c92e507ea407978520867b6cf5fe57e520f8f6
rld-SQA <S00320594@acad.tri-c.edu>
2025-07-03 02:30:30 +0000

    Working through Lab 1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Step 1
+        // Name: Rich DeLoof
+        // Title: IT-1050-Lab 1
+
+        // Step 2 - Basic Output
+        Console.WriteLine("Rich DeLoof, " + "IT-1050 - Programming Logic");
+
+        // Step 3 - Declare and Use Variables
+        int faveNum = 12;
+        string faveProgLang = "C#";
+        double numProgs = 50.0D;
+        bool progExp = true;
+        Console.WriteLine("My favorite number is " + faveNum);
+
         string name = "Rich";
         Console.WriteLine("Hello. " + name + "!");
         Console.WriteLine("Welcome to IT-1050!");
