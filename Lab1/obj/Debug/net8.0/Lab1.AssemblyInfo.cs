0ad4165bf6def6eb022658a6c856113a25f23aea
rld-SQA <S00320594@acad.tri-c.edu>
2025-07-07 03:07:51 +0000

    Started Lab 2

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20fe4e0be7ee4658b03917259da820d2a0bc39f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a822afe8eef94099c99045734cfc2c62ec71d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Part 1: Operator Practice
+        int a = 10, b = 5, c = 15;
+        Console.WriteLine(a > b);
+        Console.WriteLine(a < c);
+        Console.WriteLine(a > b && a > c);
+        Console.WriteLine("Hello, World!");
+    }
+}
