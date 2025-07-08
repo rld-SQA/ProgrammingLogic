9fdf6736d5d2feb3ed1c3a41ffc8d51e23c5199f
rld-SQA <S00320594@acad.tri-c.edu>
2025-07-08 01:50:38 +0000

    Lab 1 continued

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,7 +6,8 @@ class Program
     {
         // Part 1: Operator Practice
         int a = 10, b = 5, c = 15;
-        Console.WriteLine(a > b);
+        Console.WriteLine("a = " + a + ", b = " + b);
+        Console.WriteLine("a greater than b? " + (a > b));
         Console.WriteLine(a < c);
         Console.WriteLine(a > b && a > c);
         Console.WriteLine("Hello, World!");
