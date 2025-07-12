8502ed6a17eccbbefe45f025b9492dcba1501be5
rld-SQA <S00320594@acad.tri-c.edu>
2025-07-12 17:16:24 +0000

    Lab 2 complete

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fa159b9e02b541979a14d2e2128ffc9f75de4aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a794a4f28ff824e562836420fc5468fcbc397ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
