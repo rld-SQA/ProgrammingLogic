bcb47f1683fabf24bf592f754d00e699ec2f5d22
rld-SQA <S00320594@acad.tri-c.edu>
2025-07-22 01:32:57 +0000

    Lab 4 in work

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d90aa3b8a332ba489a4033c01bccddcd39e4c5bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686803143f88ed886f8218ad793996aba6099ecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        /* Name: Rich DeLoof
+        Title: IT-1050 - Lab 4 */
+
+        // Provided starting code
+
+        /* The code block below inserted here to run two test cases for Discussion 4 - Software Testing
+        Console.Write("Enter the first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = num1 + num2;
+
+        Console.WriteLine("The sum is: " + sum);
+        */
     }
 }
